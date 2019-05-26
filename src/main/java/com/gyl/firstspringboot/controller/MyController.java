916671ac04ea3b23cc78e1d6e5bd34a8b68752c3
tuesunporc <tuesunporc@gmail.com>
2019-05-27 01:27:30 +0800

    Initial commit

---
+++ src/main/java/com/gyl/firstspringboot/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.gyl.firstspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello springboot!!!";
+    }
+}
